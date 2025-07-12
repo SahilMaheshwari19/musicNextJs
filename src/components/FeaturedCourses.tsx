5f09cb73857f73408b49a3c7f8563351858e8f9b
SahilMaheshwari19 <sahil19singhi@gmail.com>
2025-07-12 15:42:05 +0530

    fix bg color

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -19,7 +19,7 @@ const FeaturedCourses = () => {
   );
 
   return (
-    <div className="py-12 bg-gray-900">
+    <div className="py-12 bg-gray-700">
       <div>
         <div className="text-center">
           <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">
